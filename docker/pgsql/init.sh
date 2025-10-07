f27a12bfd2b6d5b29f4ec6853521ea69b2e18168
Abel BRIEN <abel.brien@gmail.com>
2025-10-07 15:47:43 +0200

    Update postgres init script in order to add contact table creation

--- docker/pgsql/init.sh
+++ docker/pgsql/init.sh
@@ -10,6 +10,26 @@ function create_database_grant_privilege() {
             CREATE DATABASE $database;
             GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
+
+    if [[ $database != 'umami' ]]; then   
+        psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+            \c $database
+
+            CREATE TABLE IF NOT EXISTS contact (
+                id integer GENERATED ALWAYS AS IDENTITY NOT NULL,
+                email varchar(255) NOT NULL,
+                name varchar(128) NOT NULL,
+                phone varchar(10) DEFAULT NULL::character varying,
+                message text NOT NULL,
+                subject varchar(128),
+                created_at timestamp without time zone DEFAULT NOW()::timestamp without time zone,
+                mail_delivered boolean DEFAULT false,
+                PRIMARY KEY (id)
+            );
+
+            COMMENT ON COLUMN contact.created_at IS '(DC2Type:datetime_immutable)';
+EOSQL
+    fi
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
