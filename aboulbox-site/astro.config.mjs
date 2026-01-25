74e8981ece5982941aac5b26e2cf989d885df5cb
Abel BRIEN <abel.brien@gmail.com>
2026-01-25 20:16:03 +0100

    Fix umami website code

--- aboulbox-site/astro.config.mjs
+++ aboulbox-site/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "",
+  base: "/",
   site: import.meta.env.SERVER_NAME,
   trailingSlash: "never",
   integrations: [react()],
