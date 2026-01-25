3b093b1d56532a9ef5b7caef8089d9474cacf2e2
Abel BRIEN <abel.brien@gmail.com>
2026-01-25 17:54:26 +0100

    Fix some issues with astro

--- aboulbox-site/astro.config.mjs
+++ aboulbox-site/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   base: "",
+  site: import.meta.env.SERVER_NAME,
   trailingSlash: "never",
   integrations: [react()],
   server: { port: 3000, host: true },
