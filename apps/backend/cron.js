e147eb468845531a9ce32d7747fed62829ab0806
Abel BRIEN <abel.brien@gmail.com>
2026-02-07 23:57:59 +0100

    perf(docusaurus): ⚡️ docusaurus is now served directly by Caddy as a static site

--- apps/backend/cron.js
+++ apps/backend/cron.js
@@ -2,12 +2,12 @@ import cron from "node-cron";
 import { pool } from "./db.js";
 import { sendMail } from "./routes/contact.js";
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   async function processUndeliveredMails() {
     console.log("[CRON] Checking for undelivered mails...");
     try {
       const { rows } = await pool.query(
-        "SELECT * FROM contact WHERE mail_delivered = false"
+        "SELECT * FROM contact WHERE mail_delivered = false",
       );
       for (const contact of rows) {
         // Try to resend mail
